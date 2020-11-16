94fbc91b96a3220663d541f28ddc3cb927b27385
FINNSEEFLY <matmak122@gmail.com>
2020-11-16 16:45:50 +0300

    All in one program now

--- DLL Injection/Implementer/Implementer/Implementer.cpp
+++ DLL Injection/Implementer/Implementer/Implementer.cpp
@@ -10,30 +10,32 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	DWORD pid;
 	HANDLE targetProcess;
-	const char* dllPath = "../../InjectorDll/Debug/InjectorDll.dll";
-	const char* injectDecoratedName = "?inject@@YGXKV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@0@Z";
+	auto dllPath = "../../InjectorDll/Debug/InjectorDll.dll";	
+	auto injectDecoratedName = "?inject@@YGXKV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@0@Z";
+	
 	std::cout << "Введите номер процесса для внедрения: ";
 	std::cin >> pid;
 
 	targetProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 	if (targetProcess) {
-		LPVOID LoadLibraryAStartAddr = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
-		LPVOID DllPathRemoteParam = (LPVOID)VirtualAllocEx(targetProcess, NULL, strlen(dllPath) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		auto LoadLibraryAStartAddr = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA");
+		auto DllPathRemoteParam = (LPVOID)VirtualAllocEx(targetProcess, NULL, strlen(dllPath) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 		WriteProcessMemory(targetProcess, DllPathRemoteParam, dllPath, strlen(dllPath) + 1, NULL);
-		HANDLE remoteThread = CreateRemoteThread(targetProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryAStartAddr, DllPathRemoteParam, 0, NULL);
+		auto remoteThread = CreateRemoteThread(targetProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryAStartAddr, DllPathRemoteParam, 0, NULL);
 		WaitForSingleObject(remoteThread, INFINITE);
 		std::cout << "DLL внедрена" << std::endl;
 		CloseHandle(remoteThread);
 		CloseHandle(targetProcess);
-		Sleep(1000);
+		fseek(stdin, 0, SEEK_SET);
+		std::cin.get();
 		inject(pid, "You were hacked", "static hack");
-		Sleep(2000);
-		HMODULE HModule = LoadLibrary(L"../../InjectorDll/Debug/InjectorDll.dll");
+		std::cin.get();
+		auto HModule = LoadLibrary(L"../../InjectorDll/Debug/InjectorDll.dll");
 		typedef void __stdcall Tinject(DWORD pid, std::string target, std::string replacement);
 		if (HModule != 0) {
-			Tinject* injectFromDll = (Tinject*)GetProcAddress(HModule, injectDecoratedName);
-			if (injectFromDll == 0) {
-				int x = GetLastError();
+			auto injectFromDll = (Tinject*)GetProcAddress(HModule, injectDecoratedName);
+			if (injectFromDll == nullptr) {
+				const int x = GetLastError();
 				std::cout << x;
 			}
 			else {
--- DllNew/DllNew/DLLdefine.h
+++
@@ -1,5 +0,0 @@
-#ifdef COMPILE_DLL_XHTYE102474
-_declspec(dllexport) int __stdcall Min(int x, int y);
-#else
-_declspec(dllimport) int __stdcall Min(int x, int y);
-#endif
